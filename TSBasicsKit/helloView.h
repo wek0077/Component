9ea16a0071c6c4c8d4f28e0cb50e34f29b4bd590
陈伟光 <wekchen@tupperware.com>
2020-05-15 15:09:56 +0800

    12

---
+++ TSBasicsKit/helloView.h
@@ -0,0 +1,17 @@
+//
+//  helloView.h
+//  ComponentTest
+//
+//  Created by admin on 2020/5/15.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface helloView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TSBasicsKit/helloView.m
@@ -0,0 +1,21 @@
+//
+//  helloView.m
+//  ComponentTest
+//
+//  Created by admin on 2020/5/15.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import "helloView.h"
+
+@implementation helloView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
